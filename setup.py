1105104a6806a3265b855fbff6dc4694cb55f8ad
MaxEhrhart <maxserhard@gmail.com>
2023-02-09 18:01:40 -0300

    directory structure and s3 functions

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # encoding: latin1
 # import os
-from setuptools import setup  # , find_packages
+from setuptools import setup, find_packages
 
 # BUILD_ID = os.environ.get("BUILD_BUILDID", "0")
 setup(
     name="aws_databricks_toolkit",
     version="0.0.1",
     description="A simple library of functions for an aws databricks cloud.",
+
     # # Author details
     author="Max",
-    # author_email="tdb",
-    # packages=find_packages("src"),
+    author_email="",
+    # packages=find_packages("aws_databricks_toolkit"),
     # package_dir={"": "src"},
     packages=['aws_databricks_toolkit'],
     setup_requires=["boto3"],
